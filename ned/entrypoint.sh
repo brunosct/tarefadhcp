f87bc63c94691b2d4e34380d4eb8b57c0f088771
Bruno <bruno@iessanclemente.net>
2025-11-24 10:19:55 +0100

    dhcp ddns

--- ned/entrypoint.sh
+++ ned/entrypoint.sh
@@ -5,7 +5,8 @@ ip route add default via 192.168.10.254
 
 rm -f /var/run/kea/kea-dhcp4.kea-dhcp4.pid
 rm -f /var/run/kea/kea-dhcp-ddns.kea-dhcp-ddns.pid
-kea-dhcp-ddns -c /etc/kea/kea-dhcp-ddns.conf -d &
-kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d &
 
-sleep infinity
+kea-dhcp-ddns -d -c /etc/kea/kea-dhcp-ddns.conf  &
+
+kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d 
+
